e75a219354a138e418748bc3deb077f0775799a2
C.Lebios <lirazanjircily@gmail.com>
2023-06-12 11:08:09 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+function GoToPage() {
+    window.location.href = 'Duxstyle-section.html';
+}
+
+function GoToDuxstyle() {
+    window.open("https://duxstyle.com/", "_blank");
+}
+
+function GoToCallbox() {
+    window.location.href = "Callbox-section.html";
+}
+
+function VisitCallbox() {
+    window.open("https://web.facebook.com/callboxsiargao/?_rdc=1&_rdr", "_blank");
+}
+
+function ReturnPage() {
+    window.location.href = "index.html"
+}
+
+function GoToMedline() {
+    window.location.href = "Medline-section.html"
+}
+
+function VisitMedline() {
+    window.open("https://www.medline.com/", "_href");
+}
+
+function GoToVXI() {
+    window.location.href = "VXI-section.html";
+}
+
+function visitVXI() {
+    window.open("https://vxi.com/");
+}
+function GoToMDR() {
+    window.location.href = "Madrabbit-section.html"
+}
+
+function visitMDR() {
+    window.open("https://www.madrabbit.com/", "_blank");
+}
+
+function GoToProject() {
+    window.location.href = "/Projects/Project-section.html";
+}
+
+function PlayTB() {
+    window.location.href = "/Projects/Games/tower_game-master/index.html";
+}
+
+function PjToMain() {
+    window.location.href = "../index.html"
+}
+
+function PlayRG() {
+    window.location.href = "/Projects/Games/HexGL/index.html";
+}
+
