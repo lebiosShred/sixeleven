510347cb3d65276d2e4330f062d4a743bda2ee80
C.Lebios <lirazanjircily@gmail.com>
2023-06-24 09:15:42 +0800

    push

--- script.js
+++ script.js
@@ -54,6 +54,57 @@ function PjToMain() {
 }
 
 function PlayRG() {
-    window.location.href = "/Projects/Games/HexGL/index.html";
+    window.location.href = "/Projects/Games/Cj/index.html";
 }
 
+function GoToCert() {
+    window.location.href = "Certifications-section.html"
+}
+
+function GoToCoursera() {
+    window.open('https://www.coursera.org/account/accomplishments/verify/PJXTAGHLCZYG', '_blank');
+
+}
+
+function GoToHub1() {
+    window.open('https://app.hubspot.com/academy/achievements/9plsqmbt/en/1/cj-amiel-lebios/frictionless-sales', '_blank');
+}
+
+function GoToHub2() {
+    window.open('https://app.hubspot.com/academy/achievements/p09cllzf/en/1/cj-amiel-lebios/hubspot-sales-software', '_blank');
+}
+
+function GoToHub3() {
+    window.open('https://app.hubspot.com/academy/achievements/450xwmhq/en/1/cj-amiel-lebios/inbound', '_blank');
+}
+
+function GoToHub4() {
+    window.open('https://app.hubspot.com/academy/achievements/m0ytz877/en/1/cj-amiel-lebios/inbound-sales', '_blank');
+}
+
+function GoToHub5() {
+    window.open('https://app.hubspot.com/academy/achievements/j4l6fq70/en/1/cj-amiel-lebios/seo', '_blank');
+}
+
+function GoToSL() {
+    window.open('https://www.sololearn.com/certificates/CT-4CVQ3AVS', '_blank');
+}
+
+function OpenCv() {
+    window.location.href = "/misc/Resume.pdf";
+}
+
+if (window.innerWidth <= 1300) {
+    alert("Hello! Your screen is not large enough to view this. Please switch to a computer, Thank you!");
+  }
+  
+function GoToLinkedIn() {
+    window.open('https://www.linkedin.com/in/c-lebios-45399a253/', '_blank');
+}
+function GoToGithub() {
+    window.open('https://github.com/lebiosShred', '_blank');
+}
+
+function sendEmail() {
+    window.location.href = "mailto:c.lebios1998@gmail.com";
+  }
